321d279a97b328af4c6df37426240cf1426e6eee
luizaantolini <luizaantolini@gmail.com>
2026-03-25 10:28:18 -0300

    Condicional aninhada com operador IF

--- condicao - aninhada - com - IF/script.js
+++ condicao - aninhada - com - IF/script.js
@@ -1,13 +1,13 @@
-let renda = Number(prompt("Digite sua renda:"));
-// ! símbolo de negação
-//isNaN: is not number (verifica se NÃO é um número)
-if (!isNaN(renda)){
+let renda = Number(prompt("digite sua renda"));
+// ! Símbolo de negação 
+// isNaN: Is not a number (verifica se NÃO é um número)
+if(!isNaN(renda)){
     if (renda >= 3000) {
-        let nomeLimpo = prompt("Nome limpo? (sim ou não)");
-        if (nomeLimpo === "sim"){
-            alert("Financiamento aprovado");
+        let nomelimpo = prompt ("Nome limpo?(sim ou não)");
+        if (nomelimpo === "sim") {
+            alert ("Financiamento aprovado");
         } else {
-            alert("Nome negativado");
+            alert ("Nome negativado");
         }
     } else {
         alert("Renda insuficiente");
